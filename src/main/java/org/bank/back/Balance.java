c57bd6f881336c8d3498161ad4678f6d3230f4d0
korolasii <timofei.kropachev@gmail.com>
2023-12-05 01:28:11 +0200

    add getBalance to class Balance

--- src/main/java/org/bank/back/Balance.java
+++ src/main/java/org/bank/back/Balance.java
@@ -1,4 +1,14 @@
 package org.bank.back;
-
+import java.util.HashMap;
 public class Balance {
+    protected static HashMap<String, Integer> balance = new HashMap<>();
+
+    public static int getBalance(String name){
+        if (balance.containsKey(name)) {
+            return balance.get(name);
+        } else {
+            balance.put(name, 0);
+            return balance.get(name);
+        }
+    }
 }
