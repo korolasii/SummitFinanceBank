991bb26add5b32e40dccb3009b1a9eea26c3a1c1
korolasii <107001703+korolasii@users.noreply.github.com>
2023-12-06 01:51:42 +0200

    add test to work with JSON

--- src/test/java/org/bank/MainTest.java
+++ src/test/java/org/bank/MainTest.java
@@ -14,5 +14,11 @@ public class MainTest {
         int expected = 0;
         int result = balances.getBalance(name);
         assertEquals(expected, result);
+
+        Map<String, Integer> resultMap = WorkWithJSON.loadBalanceFromJson();
+        assertTrue(!resultMap.isEmpty());
+        assertEquals(Optional.ofNullable(expected), Optional.ofNullable(resultMap.get(name)));
+
+
     }
 }
