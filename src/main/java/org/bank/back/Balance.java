0c98aaf0faa59d4d6089e5ffbdffe207fdd83c4b
korolasii <107001703+korolasii@users.noreply.github.com>
2023-12-06 01:36:37 +0200

    Update balance for work with JSON

--- src/main/java/org/bank/back/Balance.java
+++ src/main/java/org/bank/back/Balance.java
@@ -1,14 +1,14 @@
 package org.bank.back;
 import java.util.HashMap;
 public class Balance {
-    protected static HashMap<String, Integer> balance = new HashMap<>();
+    protected static Map<String, Integer> balance = WorkWithJSON.loadBalanceFromJson();
 
-    public static int getBalance(String name){
-        if (balance.containsKey(name)) {
-            return balance.get(name);
-        } else {
-            balance.put(name, 0);
-            return balance.get(name);
+    public static int getBalance(String name) {
+        if (!balance.containsKey(name)) {
+            WorkWithJSON.addNewPerson(name);
+            balance = WorkWithJSON.loadBalanceFromJson();
         }
+        return balance.get(name);
     }
+
 }
