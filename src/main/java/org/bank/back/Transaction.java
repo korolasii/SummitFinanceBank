84fe25d2cb5f147c0602b8c3e2c005b9f104dc3b
otval-games <modernogamesstudio@gmail.com>
2023-12-08 22:42:27 +0200

    - Transaction added

--- src/main/java/org/bank/back/Transaction.java
+++ src/main/java/org/bank/back/Transaction.java
@@ -1,4 +1,45 @@
 package org.bank.back;
 
 public class Transaction {
+    private String type;
+    private float amount;
+    private String sender;
+    private String adress;
+
+    Transaction(String type, float amount){
+        this.type=type;
+        this.amount=amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
 }
